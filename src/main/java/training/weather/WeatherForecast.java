be677e750ed4be0bd03aea38b780ef9095ac22d1
rogersm92 <roger.serra92@gmail.com>
2021-05-17 17:28:04 +0200

    testing using the model works

--- src/main/java/training/weather/WeatherForecast.java
+++ src/main/java/training/weather/WeatherForecast.java
@@ -9,17 +9,18 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import training.model.City;
 
 public class WeatherForecast {
 
-	public String getCityWeather(String city, Date datetime) throws IOException {
-		if (datetime == null) {
-			datetime = new Date();
+	public String getCityWeather(City city) throws IOException {
+		if (city.getDatetime() == null) {
+			city.setDatetime(new Date());
 		}
-		if (datetime.before(new Date(new Date().getTime() + (1000 * 60 * 60 * 24 * 6)))) {
+		if (city.getDatetime().before(new Date(new Date().getTime() + (1000 * 60 * 60 * 24 * 6)))) {
 			HttpRequestFactory rf = new NetHttpTransport().createRequestFactory();
 			HttpRequest req = rf
-				.buildGetRequest(new GenericUrl("https://www.metaweather.com/api/location/search/?query=" + city));
+				.buildGetRequest(new GenericUrl("https://www.metaweather.com/api/location/search/?query=" + city.getName()));
 			String r = req.execute().parseAsString();
 			JSONArray array = new JSONArray(r);
 			String woe = array.getJSONObject(0).get("woeid").toString();
@@ -28,7 +29,7 @@ public class WeatherForecast {
 			r = req.execute().parseAsString();
 			JSONArray results = new JSONObject(r).getJSONArray("consolidated_weather");
 			for (int i = 0; i < results.length(); i++) {
-				if (new SimpleDateFormat("yyyy-MM-dd").format(datetime)
+				if (new SimpleDateFormat("yyyy-MM-dd").format(city.getDatetime())
 					.equals(results.getJSONObject(i).get("applicable_date").toString())) {
 					return results.getJSONObject(i).get("weather_state_name").toString();
 				}
--- src/test/java/training/weather/WeatherForecastTest.java
+++ src/test/java/training/weather/WeatherForecastTest.java
@@ -5,13 +5,27 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 import org.junit.Test;
+import training.model.City;
 
 public class WeatherForecastTest {
 
+	/*
 	@Test
 	public void unfinished_test() throws IOException {
 		WeatherForecast weatherForecast = new WeatherForecast();
 		String forecast = weatherForecast.getCityWeather("Madrid", new Date());
 		System.out.println(forecast);
 	}
+
+	 */
+
+	@Test
+	public void model_test() throws IOException {
+		City madrid = new City("Madrid", new Date());
+		WeatherForecast weatherForecast = new WeatherForecast();
+		String forecast = weatherForecast.getCityWeather(madrid);
+		madrid.setWeather(forecast);
+		System.out.println(madrid.getWeather());
+	}
+
 }
