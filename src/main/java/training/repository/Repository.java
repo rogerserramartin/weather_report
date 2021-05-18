fe8846615d51fcff7e264cfe7ca80f5f0f046cc4
rogersm92 <roger.serra92@gmail.com>
2021-05-18 17:31:30 +0200

    forgot to add this file

---
+++ src/main/java/training/repository/Repository.java
@@ -0,0 +1,73 @@
+package training.repository;
+
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import training.model.City;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+public class Repository {
+
+    private static final String QUERY_URL = "https://www.metaweather.com/api/location/search/?query=";
+    private static final String LOCATION_URL = "https://www.metaweather.com/api/location/";
+
+    public Repository() {
+    }
+
+    public String getWeather(City city) {
+
+        // HTTP request
+        HttpRequestFactory requestFactory = new NetHttpTransport().createRequestFactory();
+        HttpRequest httpRequest = null;
+        try {
+            httpRequest = requestFactory.buildGetRequest(new GenericUrl(QUERY_URL + city.getName()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //GETTING A RESPONSE
+        String response = null;
+        try {
+            assert httpRequest != null;
+            response = httpRequest.execute().parseAsString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //SOME VARIABLES TO AVOID HARDCODING
+        String woeid = "woeid";
+        String weatherKey = "consolidated_weather";
+        String datePattern = "yyyy-MM-dd";
+        String applicableDate = "applicable_date";
+        String weatherStateName = "weather_state_name";
+        String weather_forecast = "";
+        int index = 0;
+
+        //GETTING A JSON
+        assert response != null;
+        JSONArray array = new JSONArray(response);
+        String woe = array.getJSONObject(index).get(woeid).toString();
+        requestFactory = new NetHttpTransport().createRequestFactory();
+        try {
+            httpRequest = requestFactory.buildGetRequest(new GenericUrl(LOCATION_URL + woe));
+            response = httpRequest.execute().parseAsString();
+            JSONArray results = new JSONObject(response).getJSONArray(weatherKey);
+            for (int i = 0; i < results.length(); i++) {
+                if (new SimpleDateFormat(datePattern).format(city.getDatetime())
+                        .equals(results.getJSONObject(i).get(applicableDate).toString())) {
+                   weather_forecast = results.getJSONObject(i).get(weatherStateName).toString();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return weather_forecast;
+    }
+}
+
+
